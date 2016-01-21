87df6183a3d684b570e9c9b15e661450d6314a4b
Builtvisible <pete@builtvisible.com>
2016-01-21 10:59:37 +0000

    react image component created

---
+++ app/js/react-image.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class Image extends React.Component {
+	constructor() {
+		super();
+
+		this.renderCaption = this.renderCaption.bind(this);
+	}
+
+	renderCaption(caption) {
+		return React.createElement("figcaption", { key: this.props.key + 'Caption' }, this.props.caption)
+	}
+
+	render() {
+		var classes = (typeof this.props.className !== 'undefined') ? this.props.className : '';
+		var alt = (typeof this.props.alt !== 'undefined') ? this.props.alt : '';
+		var caption = (typeof this.props.caption !== 'undefined' && this.props.caption !== '') ? this.renderCaption() : '';
+
+		return (
+			React.createElement("div", { className: 'media_widget ' + classes, key: this.props.key },
+				React.createElement("figure", { className: 'image_container' },
+					React.createElement("img", { src: this.props.src, alt: alt }),
+					caption
+				)
+			)
+		)
+	}
+}
