313ec67b12891338f5830330957ca0bdc53584e9
Pete Watson-Wailes <pete@watson-wail.es>
2024-01-08 15:45:15 +0000

    fixes

--- app/routes/core-routes.js
+++ app/routes/core-routes.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import ReactDOMServer from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import JsonData from '../json/data.json';
 import FMttM from '../js/conductor.js';
 
-// React.renderToString takes your component and generates rendered markup. SEO friendliness all the way
 export default function(app) {
   app.get('/', (req, res) => {
-    res.render('index.ejs', {
-      reactTitle: JsonData.home.title,
-      reactSection: '',
-      reactDescription: JsonData.home.description,
-      reactOutput: ReactDOMServer.renderToString(<FMttM {...JsonData.home} />)
-    });
+  	const reactTitle = JsonData.home.title,
+  				reactDescription = JsonData.home.description,
+  				reactSection = JsonData[req.params]
+
+  	const reactOutput = renderToString(<FMttM {...JsonData.home} />)
+
+    res.render('index.ejs', { reactTitle, reactSection, reactDescription, reactOutput });
   });
 
   app.get('/:section', (req, res) => {
-    res.render('index.ejs', {
-      reactTitle: JsonData[req.params.section].title,
-      reactSection: req.params.section,
-      reactDescription: JsonData[req.params.section].description,
-      reactOutput: ReactDOMServer.renderToString(<FMttM {...JsonData[req.params.section]} />)
-    });
+  	const reactTitle = JsonData[req.params.section].title,
+  				reactDescription = JsonData[req.params.section].description,
+  				reactSection = req.params.section
+
+  	const reactOutput = renderToString(<FMttM {...JsonData[req.params.section]} />)
+
+    res.render('index.ejs', { reactTitle, reactSection, reactDescription, reactOutput });
   });
 }
